8c617837319b87946794b64b28ab4af3e1252305
Bhargavi Lukalapu <bhargavilukalapu@gmail.com>
2026-04-30 18:21:46 -0800

    Displayed list of books and its properties using ngFor directive

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { Book } from '../book';
+import { NgFor } from "../../../node_modules/@angular/common";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent{
- book:Book = {id : 1,title : "Title one", author: "Author one"}
+ books:Book[]=[
+  {id : 1, author : "Author one", title : "Title one"},
+  {id : 2, author : "Author Two", title : "Title Two"},
+  {id : 3, author : "Author three", title : "Title three"}
+ ]
 }
 
